c2eb3951c4023a540a09cfd50bcafa5a533ae172
AmagiDDmxh <1107537248@qq.com>
2017-05-29 14:59:21 +0800

    Fix the status change transition effect

--- src/utils/ljsign.js
+++ src/utils/ljsign.js
@@ -1,48 +1,49 @@
-import CryptoJS from 'crypto-js'
-
 
 // 登录钱包
-export const decrypt = (enckey, pwd) => CryptoJS.AES.decrypt(enckey, pwd).toString(CryptoJS.enc.Utf8)
+export function decrypt(enckey, pwd) {
+    //解密私钥
+    var prvkey = CryptoJS.AES.decrypt(enckey, pwd).toString(CryptoJS.enc.Utf8);
+    return prvkey;
+}
 
 export function doSign(prvkey, msg) {
-    // 签名
-  var sig = new KJUR.crypto.Signature({
-    'alg': 'SHA256withECDSA'
-  })
-  sig.initSign({
-    'ecprvhex': prvkey,
-    'eccurvename': 'secp256r1'
-  })
-  sig.updateString(msg)
-
-  const sigval = sig.sign()
-  return sigval
+    //签名
+    var sig = new KJUR.crypto.Signature({
+        'alg': 'SHA256withECDSA'
+    });
+    sig.initSign({
+        'ecprvhex': prvkey,
+        'eccurvename': 'secp256r1'
+    });
+    sig.updateString(msg);
+    var sigval = sig.sign();
+    return sigval;
 }
 
 export function doVerify(pubkey, msg, sigval) {
-    // 验证签名
-  let sig = new KJUR.crypto.Signature({
-    'alg': 'SHA256withECDSA',
-    'prov': 'cryptojs/jsrsa'
-  })
-  sig.initVerifyByPublicKey({
-    'ecpubhex': pubkey,
-    'eccurvename': 'secp256r1'
-  })
-
-  sig.updateString(msg)
-
-  return sig.verify(sigval)
+    //验证签名
+    var sig = new KJUR.crypto.Signature({
+        "alg": 'SHA256withECDSA',
+        "prov": "cryptojs/jsrsa"
+    });
+    sig.initVerifyByPublicKey({
+        'ecpubhex': pubkey,
+        'eccurvename': 'secp256r1'
+    });
+    sig.updateString(msg);
+    var result = sig.verify(sigval);
+    return result;
 }
 
 export function doValidatePwd(prvkey, pubkey) {
-    // 验证密码
-    // 返回true,说明用户输入的密码正确
-    // 返回false,说哦名密码有误
-  if (prvkey.length === 0) return false
-  const msg = 'aaa'
-  const sigval = doSign(prvkey, msg)
-  return doVerify(pubkey, msg, sigval)
+    //验证密码
+    //返回true,说明用户输入的密码正确
+    //返回false,说哦名密码有误
+    if (0 == prvkey.length) return false;
+    var msg = 'aaa';
+    var sigval = doSign(prvkey, msg);
+    var result = doVerify(pubkey, msg, sigval);
+    return result;
 }
 
 export function genKeyPairHex() {
